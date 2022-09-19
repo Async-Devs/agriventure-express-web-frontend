453757bcd6f3a46f7a808f9cc0988116770bb59d
Achira Dias <achira828@gmail.com>
2022-09-20 01:11:39 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import {Routes,Route,BrowserRouter} from "react-router-dom";
 
 import Dashboard from "../pages/dashboard/dashboard";
-
+/*
+* Order Inventory Subsystem - Achira
+* Account Management/ Support Subsystem - Toxic Supun
+* Data Aggregation and Visualization - Ransika
+* */
 
 function AppRouter(){
 
