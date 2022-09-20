e80c14448e561d92ceb502ae253b3051fdcab9b4
Achira Dias <achira828@gmail.com>
2022-09-20 13:03:40 +0530

    Add code styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import AppRouter from "./router";
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <AppRouter />
-    </div>
-  );
+	return (
+		<div className="App">
+			<AppRouter/>
+		</div>
+	);
 }
 
 export default App;
