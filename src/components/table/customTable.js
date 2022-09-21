bc377619fbf2fd85231bf0078fb7cc1f2c2159cd
Achira Dias <achira828@gmail.com>
2022-09-22 01:21:22 +0530

    WIP: Create common table

--- src/components/table/customTable.js
+++ src/components/table/customTable.js
@@ -1,13 +1,95 @@
-import React from "react";
-import { Table } from "@mui/material";
+// import * as React from "react";
+// import { DataGrid } from "@mui/x-data-grid";
+//
+// const columns = [
+// 	{ field: "id", headerName: "ID", width: 70 },
+// 	{ field: "firstName", headerName: "First name", width: 130 },
+// 	{ field: "lastName", headerName: "Last name", width: 130 },
+// 	{
+// 		field: "age",
+// 		headerName: "Age",
+// 		type: "number",
+// 		width: 90,
+// 	},
+// ];
+//
+// const rows = [
+// 	{ id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+// 	{ id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+// 	{ id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+// 	{ id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+// 	{ id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+// 	{ id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+// 	{ id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+// 	{ id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+// 	{ id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+// ];
+//
+// export default function DataTable() {
+// 	return (
+// 		<div style={{ height: 400, width: "100%" }}>
+// 			<DataGrid
+// 				rows={rows}
+// 				columns={columns}
+// 				pageSize={5}
+// 				rowsPerPageOptions={[5]}
+// 				checkboxSelection={true}
+// 			/>
+// 		</div>
+// 	);
+// }
 
-function CustomTable(){
-	return(
-		<div>
-			<h2>Table heading</h2>
-			<Table/>
-		</div>
-	);
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(name, calories, fat, carbs, protein) {
+	return { name, calories, fat, carbs, protein };
 }
 
-export default CustomTable;
+const rows = [
+	createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+	createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+	createData("Eclair", 262, 16.0, 24, 6.0),
+	createData("Cupcake", 305, 3.7, 67, 4.3),
+	createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+export default function BasicTable() {
+	return (
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell>Dessert (100g serving)</TableCell>
+						<TableCell align="right">Calories</TableCell>
+						<TableCell align="right">Fat&nbsp;(g)</TableCell>
+						<TableCell align="right">Carbs&nbsp;(g)</TableCell>
+						<TableCell align="right">Protein&nbsp;(g)</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows.map((row) => (
+						<TableRow
+							key={row.name}
+							sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+						>
+							<TableCell component="th" scope="row">
+								{row.name}
+							</TableCell>
+							<TableCell align="right">{row.calories}</TableCell>
+							<TableCell align="right">{row.fat}</TableCell>
+							<TableCell align="right">{row.carbs}</TableCell>
+							<TableCell align="right">{row.protein}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+}
