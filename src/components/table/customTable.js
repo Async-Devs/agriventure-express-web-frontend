1a5710d8754d8aeffea90d2f8a01326abf019742
Achira Dias <achira828@gmail.com>
2022-09-21 22:49:35 +0530

    Create customTable.js

---
+++ src/components/table/customTable.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Table } from "@mui/material";
+
+function CustomTable(){
+	return(
+		<div>
+			<h2>Table heading</h2>
+			<Table/>
+		</div>
+	);
+}
+
+export default CustomTable;
