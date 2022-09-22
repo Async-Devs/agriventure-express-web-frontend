5296e3df5b464715e17831953c440dbfe0dac6f7
RansikaC99 <89589832+RansikaC99@users.noreply.github.com>
2022-09-23 00:10:34 +0530

    bar scroll x hide

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -152,6 +152,7 @@ function ResponsiveDrawer(props) {
 					{drawer}
 				</Drawer>
 			</Box>
+			
 		</Box>
 	);
 }
--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,28 +1,22 @@
 import React from "react";
-import {useState} from "react";
-import style from "./dashboard.module.css"
+import "./dashboard.module.css"
 import Sidebar from "../../components/Sidebar.js"
 
 function Dashboard(){
 
     // states
-    const [name,setName] = useState("Ransika");
 
 
     //data access from axios
 
     //function
-    function changeName(){
-        setName("Achira");
-    }
 
 
     return(
         <div>
             <Sidebar/>
-            <div>{name}</div>
-            <div className={style}>
-                <button onClick={changeName}>Change Name</button>
+            <div >waaaw</div>
+            <div >
                 <img src={"https://lh4.googleusercontent.com/yVQ4QNgv8jAOZ-06Vpgvvyq6M_Df-CaKewWhE6cwJ-QMUFz-Iz3JIhy_IiAbgre9Rpc=w2400"} alt="sample" width="80%"/>
             </div>
         </div>
