8f3d6252f365a13884a4724d52fcca330a6b8de8
Achira Dias <achira828@gmail.com>
2022-09-24 04:11:09 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function ErrorPage(){
-    return(
-        <div>404 error</div>
-    );
+	return(
+		<div>
+			<h3>404 page not found</h3>
+			<p>We are sorry but the page you are looking for does not exist.</p>
+		</div>
+	);
 }
 
 export default ErrorPage;
