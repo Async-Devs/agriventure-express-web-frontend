36512b83b7862769e479fab71e411dff0773bf2d
Supun Dasanayaka <supun.19@cse.mrt.ac.lk>
2022-09-24 02:41:48 +0530

    Refund requests - customer

--- src/pages/orderView.js
+++ src/pages/orderView.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import {Grid} from "@mui/material";
+import Button from "@mui/material/Button";
+import RefundRequest from "../components/refundRequest/refundRequest";
 
 function OrderView(){
+	const [open,setOpen] = useState(false);
+	const [refundBody,setRefundBody] = useState();
+	const [refundValue,setRefundValue] = useState();
+
+	function onClickRefund(){
+		setOpen(true);
+	}
+
+	function handleClose(){
+		setOpen(false);
+	}
+
+	function handleSubmit(){
+		//todo: check the validity
+		handleClose();
+
+	}
+
+	function onChange(event){
+		if(event.target.name === "body"){
+			setRefundBody(event.target.value);
+		}else if(event.target.name === "value"){
+			// if(!isNaN(event.target.value) && !isNaN(parseFloat(event.target.value)) ){
+			// 	setRefundValue(parseFloat(event.target.value));
+			// }
+			setRefundValue(parseFloat(event.target.value));
+		}
+	}
+
 	return(
-		<div>OrderDetails</div>
+		<Grid container>
+			<Grid item><Button onClick={onClickRefund} variant="outlined">Refund</Button></Grid>
+			<Grid item><RefundRequest open={open} handleSubmit={handleSubmit} handleClose={handleClose} onChange={onChange} body={refundBody} value={refundValue}/></Grid>
+		</Grid>
 	);
 }
 
