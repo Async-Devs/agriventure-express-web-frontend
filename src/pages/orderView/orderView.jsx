e8b280dd511c3038131b8a59c03845c384b06122
Achira Dias <achira828@gmail.com>
2022-09-24 04:21:11 +0530

    Update orderView.jsx

--- src/pages/orderView/orderView.jsx
+++ src/pages/orderView/orderView.jsx
@@ -1,13 +1,44 @@
 import React, {useState} from "react";
-// import {useParams} from "react-router-dom";
+import {Grid} from "@mui/material";
+import Button from "@mui/material/Button";
+import RefundRequest from "../../components/refundRequest/refundRequest";
+
 
 function OrderView(){
-	const [id, setId] = useState();
-	setId(2);
+	const [open,setOpen] = useState(false);
+	const [refundBody,setRefundBody] = useState();
+	const [refundValue,setRefundValue] = useState();
+
+	function onClickRefund(){
+		setOpen(true);
+	}
+
+	function handleClose(){
+		setOpen(false);
+	}
+
+	function handleSubmit(){
+		//todo: check the validity
+		handleClose();
+
+	}
+
+	function onChange(event){
+		if(event.target.name === "body"){
+			setRefundBody(event.target.value);
+		}else if(event.target.name === "value"){
+			// if(!isNaN(event.target.value) && !isNaN(parseFloat(event.target.value)) ){
+			// 	setRefundValue(parseFloat(event.target.value));
+			// }
+			setRefundValue(parseFloat(event.target.value));
+		}
+	}
+
 	return(
-		<div>OrderDetails
-			{id}
-		</div>
+		<Grid container>
+			<Grid item><Button onClick={onClickRefund} variant="outlined">Refund</Button></Grid>
+			<Grid item><RefundRequest open={open} handleSubmit={handleSubmit} handleClose={handleClose} onChange={onChange} body={refundBody} value={refundValue}/></Grid>
+		</Grid>
 	);
 }
 
