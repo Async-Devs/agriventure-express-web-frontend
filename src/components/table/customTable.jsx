e59cc096bb8b325b768ff71dbfd75d51b1254124
Achira Dias <achira828@gmail.com>
2022-09-24 04:10:51 +0530

    Update customTable.jsx

--- src/components/table/customTable.jsx
+++ src/components/table/customTable.jsx
@@ -5,17 +5,22 @@ import PropTypes from "prop-types";
 function CustomTable(props) {
 	const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-	const {rows, columns} = props;
+	const {rows, columns, enableSelectionOnRowClick, enableCheckBox} = props;
 	console.log(columns);
+	console.log(rows);
 	return (
-		<div style={{ height: 400, width: "100%" }}>
+		<div style={{ height: 400, width: "100%"}}>
 			<DataGrid
 				rows={rows}
 				columns={columns}
 				pageSize={rowsPerPage}
 				onPageSizeChange={(newPageSize) => setRowsPerPage(newPageSize)}
 				rowsPerPageOptions={[5, 10, 25, 100]}
-				checkboxSelection = {true}
+				checkboxSelection = {enableCheckBox}
+				disableSelectionOnClick = {!enableSelectionOnRowClick}
+				// rowHeight={50}
+				getRowHeight={() => "auto"}
+				disableColumnMenu={true}
 			/>
 			{/*<button onClick={handleChangeRowsPerPage}>ddd</button>*/}
 		</div>
@@ -24,7 +29,9 @@ function CustomTable(props) {
 // To define the data type of the prop for prop validation in react.
 CustomTable.propTypes = {
 	rows: PropTypes.array, // The row items of the table should come in the form of an array of objects.
-	columns: PropTypes.array
+	columns: PropTypes.array,
+	enableSelectionOnRowClick: PropTypes.bool,
+	enableCheckBox: PropTypes.bool
 };
 
 export default CustomTable;
