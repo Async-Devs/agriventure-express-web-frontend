dd759ce8ee6707a6762e5d8c6052cc8228f9b3c6
Achira Dias <achira828@gmail.com>
2022-09-24 21:19:06 +0530

    Update orders.jsx

--- src/pages/orders/orders.jsx
+++ src/pages/orders/orders.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
-import { Grid, ThemeProvider} from "@mui/material";
+import {Grid, ThemeProvider} from "@mui/material";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import theme from "../../components/theme/theme";
 import OrderTable from "./orderTable";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 
 function Orders(){
 	return(
@@ -22,18 +25,27 @@ function Orders(){
 						overflow: "auto",
 					}}
 				>
-					{/*<Toolbar />*/}
-					<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-						
-							
-						{/* Recent Orders */}
+
+
+					<Grid container spacing={0}>
 						<Grid item xs={12}>
-							<Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-								<OrderTable/>
-							</Paper>
+							<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+								<Grid item xs={12}>
+									<Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+										<h1>Orders</h1>
+										<Button variant="outlined" startIcon={<DeleteIcon/>} color={"error"}>
+									Delete
+										</Button>
+										<OrderTable/>
+									</Paper>
+								</Grid>
+
+							</Container>
+
 						</Grid>
-						
-					</Container>
+
+					</Grid>
+
 				</Box>
 			</Box>
 		</ThemeProvider>
