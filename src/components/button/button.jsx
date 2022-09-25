fdab7d45c78c1664854e5863fc799931215d4b1a
Achira Dias <achira828@gmail.com>
2022-09-25 07:36:29 +0530

    Update button.jsx

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,11 +1,24 @@
 import * as React from "react";
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
 import {Button} from "@mui/material";
+import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
 
-function SaveButton() {
+export default function SaveButton() {
 	return (
 		<Button>Save{" "}<SaveOutlinedIcon/></Button>
 	);
 }
 
-export default SaveButton;
+export function LinkedButton(props){
+	return(
+		<Link to={props.href} style={{ textDecoration: "none" }}> <Button>{props.content}</Button></Link>
+	);
+}
+
+LinkedButton.propTypes = {
+	/** The href is the path that the button directs to*/
+	href: PropTypes.string,
+	/** The content is the Button Name */
+	content: PropTypes.string
+};
