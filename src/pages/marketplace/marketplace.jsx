71ae5f92cfb921affd504a02b2e990dfcdb0eb24
Achira Dias <achira828@gmail.com>
2022-09-26 13:16:06 +0530

    Update marketplace.jsx

--- src/pages/marketplace/marketplace.jsx
+++ src/pages/marketplace/marketplace.jsx
@@ -1,31 +1,21 @@
 import React from "react";
 import { Grid, Pagination} from "@mui/material";
 import MarketplaceCard from "./marketplaceCard";
-// import CardComponent from "../../components/cardComponent/cardComponent";
 
-// const Item = styled(Paper)(({ theme }) => ({
-// 	...theme.typography.body2,
-// 	textAlign: "center",
-// 	color: theme.palette.text.secondary,
-// 	height: 60,
-// 	lineHeight: "60px",
-// }));
 function Marketplace(){
-
 	return(
-		<Grid container  border={1} justifyContent={"center"} p={5} bgcolor={"lightgray"}>
+		<Grid container justifyContent={"center"} p={5}>
 			<Pagination item count={10} shape="rounded" />
-			<Grid item container spacing={2} p={4} maxWidth={1200}  border={1} xs={12} minWidth={800}>
+			<Grid item container spacing={4} p={4} xs={12} >
 				{[1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5].map((p)=>{
 					return(
-						<Grid container key={p} item xs={12} sm={6} lg={3} xl={2} alignItems={"center"} justifyContent={"center"} border={1}>
-							<MarketplaceCard/>
+						<Grid container key={p} item xs={12} sm={6} md={4} lg={3} xl={2} justifyContent={"center"}>
+							<MarketplaceCard imgSrc={"https://images.unsplash.com/photo-1598170845058-32b9d6a5da37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"} itemName={"Carrots"} minBid={4500000} quantity={1000}/>
 						</Grid>
 					);
 				})}
 			</Grid>
 			<Pagination item count={10} shape="rounded" />
-
 		</Grid>
 	);
 }
