486eea031e743f52e43616514b7f9cb055bb5c1a
Achira Dias <achira828@gmail.com>
2022-09-26 23:45:10 +0530

    Update pagination.jsx

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -4,20 +4,20 @@ import PropTypes from "prop-types";
 import _ from "lodash";
 
 function CustomPagination (props){
-	const { itemsCount, pageSize, onPageChange} = props;
+	const { itemsCount, pageSize, onPageChange, currentPage} = props;
 	const pagesCount = Math.ceil(itemsCount / pageSize);
 	if (pagesCount === 1) return null;
 	const pages = _.range(1, pagesCount + 1);
 	return (
-		<Pagination count={pages.length} shape="rounded" onChange={(event, page)=>{onPageChange(page);}}/>
+		<Pagination count={pages.length} shape="rounded" page={currentPage} onChange={(event, page)=>{onPageChange(page);}}/>
 	);
 }
 
 CustomPagination.propTypes = {
 	itemsCount: PropTypes.number.isRequired,
 	pageSize: PropTypes.number.isRequired,
-	// currentPage: PropTypes.number.isRequired,
 	onPageChange: PropTypes.func.isRequired,
+	currentPage: PropTypes.number.isRequired
 };
 
 export default CustomPagination;
