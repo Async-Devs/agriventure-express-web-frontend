1f23d8bb87c9fdb8e87821972ae5898da584806d
Achira Dias <achira828@gmail.com>
2022-09-30 06:20:40 +0530

    Update itemServices.js

--- src/services/itemServices.js
+++ src/services/itemServices.js
@@ -1,12 +1,10 @@
-// import axios from "axios";
-//
-// // eslint-disable-next-line no-undef
-// const apiUrl = process.env.REACT_APP_API_URL;
-// const apiEndpoint = apiUrl + "/items";
-//
-// export function getAllItems(){
-// 	const itemsArray = axios.get(`${apiEndpoint}/`).then(
-// 		console.log(itemsArray)
-// 	);
-// 	return itemsArray.data;
-// }
+import Axios from "axios";
+
+// eslint-disable-next-line no-undef
+const apiUrl = process.env.REACT_APP_API_URL;
+const apiEndpoint = apiUrl + "/items";
+
+export function getAllItems(){
+	const items = Axios.get(`${apiEndpoint}`);
+	return items;
+}
