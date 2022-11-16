312853d90be9ff7c315de4d74cfaeca413f6afdb
Achira Dias <achira828@gmail.com>
2022-11-16 14:12:00 +0530

    Update producerDashboard.js

--- src/pages/producerDashboard/producerDashboard.js
+++ src/pages/producerDashboard/producerDashboard.js
@@ -6,20 +6,28 @@ import Typography from "@mui/material/Typography";
 import {CircularProgress, Divider} from "@mui/material";
 import ProducerItems from "./producerItems";
 import ProducerSales from "./producerSales";
+import authService from "../../services/auth.service";
 
 function ProducerDashboard(){
+	// eslint-disable-next-line no-unused-vars
 	const [isLoading, setLoading] = useState(true);
+	// eslint-disable-next-line no-unused-vars
 	const [myListings, setMyListing] = useState([]);
 
 	useEffect(()=>{
-
-	});
+		console.log(authService.getCurrentUserType());
+		setLoading(false);
+	},[]);
 
 	function renderMyListing(){
 		if(isLoading){
 			return(
 				<Paper>
-					<CircularProgress/>
+					<Grid container height={400} alignItems={"center"} justifyContent={"center"}>
+						<Grid item>
+							<CircularProgress/>
+						</Grid>
+					</Grid>
 				</Paper>
 			);
 		}else {
@@ -35,7 +43,7 @@ function ProducerDashboard(){
 		<Container >
 			<Grid container align={"center"}>
 				<Grid item xs={12}>
-					<Typography variant="h2"><span style={{color: "primary"}}>My Crops</span></Typography>
+					<Typography variant="h2"><span style={{color: "green"}}>My Crops</span></Typography>
 					<hr />
 				</Grid>
 
