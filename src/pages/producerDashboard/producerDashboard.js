cb0f6c748c50658cfa6fbfd92bbf1b315cfe13d1
Achira Dias <achira828@gmail.com>
2022-11-16 13:49:17 +0530

    Update producerDashboard.js

--- src/pages/producerDashboard/producerDashboard.js
+++ src/pages/producerDashboard/producerDashboard.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import {Divider} from "@mui/material";
+import {CircularProgress, Divider} from "@mui/material";
 import ProducerItems from "./producerItems";
 import ProducerSales from "./producerSales";
 
 function ProducerDashboard(){
+	const [isLoading, setLoading] = useState(true);
+	const [myListings, setMyListing] = useState([]);
+
+	useEffect(()=>{
+
+	});
+
+	function renderMyListing(){
+		if(isLoading){
+			return(
+				<Paper>
+					<CircularProgress/>
+				</Paper>
+			);
+		}else {
+			return(
+				<Paper>
+					<ProducerItems/>
+				</Paper>
+			);
+		}
+	}
+
 	return(
 		<Container >
 			<Grid container align={"center"}>
 				<Grid item xs={12}>
-					<Typography variant="h2"><span style={{color: "green"}}>My Crops</span></Typography>
+					<Typography variant="h2"><span style={{color: "primary"}}>My Crops</span></Typography>
 					<hr />
 				</Grid>
 
 				<Grid item xs={12}>
-					<Paper>
-						<ProducerItems/>
-					</Paper>
+					{renderMyListing()}
 				</Grid>
 
 				<Grid item xs={12}>
