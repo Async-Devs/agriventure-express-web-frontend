e12692c2b6b60759889344da6149c283c854a391
Achira Dias <achira828@gmail.com>
2022-11-17 12:52:18 +0530

    Update GoogleMap.jsx

--- src/components/GoogleMap/GoogleMap.jsx
+++ src/components/GoogleMap/GoogleMap.jsx
@@ -2,14 +2,16 @@ import React, {useEffect, useState} from "react";
 import {GoogleMap, LoadScript, Marker} from "@react-google-maps/api";
 import PropTypes from "prop-types";
 import authService from "../../services/auth.service";
-// eslint-disable-next-line no-undef
-let apiKey ="";
-
+import {CircularProgress, Grid} from "@mui/material";
+import Container from "@mui/material/Container";
 
 function MyComponent(props) {
+	const [apiKey, setApikey] = useState(null);
+	const [location, setLocation] = useState({lat: "x", lng: "x"});
 	useEffect(()=>{
 		const fetchData = async ()=>{
-			apiKey = await authService.getGoogleAPIkey();
+			const {data} = await authService.getGoogleAPIkey();
+			setApikey(data);
 		};
 		fetchData();
 	},[]);
@@ -24,12 +26,20 @@ function MyComponent(props) {
 		marginBottom: "20px"
 	};
 
+	const handleDrag = (event)=>{
+		const {latLng} = event;
+		const lat = latLng.lat();
+		const lng = latLng.lng();
+		props.setPointer({lat: lat, lng: lng});
+		setLocation({lat: lat, lng: lng});
+	};
 
 	const mapClick = (event)=>{
 		const {latLng} = event;
 		const lat = latLng.lat();
 		const lng = latLng.lng();
 		props.setPointer({lat: lat, lng: lng});
+		setLocation({lat: lat, lng: lng});
 		setCenter({lat: lat, lng: lng});
 	};
 
@@ -37,16 +47,31 @@ function MyComponent(props) {
 		if(location.lat==="x" && location.lng==="x"){
 			return;
 		}
+		const icon = {
+			url: "https://cdn.pixabay.com/photo/2014/04/02/10/45/poi-304466_960_720.png", // url
+			scaledSize: { height: 45, width: 30 }
+		};
 		return(
 			<Marker
-				position={props.locationPointer}
+				position={location}
+				draggable={true}
+				onDrag={handleDrag}
+				icon={icon}
 			/>
 		);
 	}
 
-	console.log(apiKey);
-
-
+	if(apiKey==null || apiKey==""){
+		return(
+			<Container>
+				<Grid container justifyContent={"center"}>
+					<Grid item>
+						<CircularProgress/>
+					</Grid>
+				</Grid>
+			</Container>
+		);
+	}
 	return (
 		<LoadScript
 			googleMapsApiKey={apiKey}
