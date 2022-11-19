5d66223f82bc92e9458bd6962cd2631b38f8e62d
Achira Dias <achira828@gmail.com>
2022-11-19 20:57:13 +0530

    Update paymentForm.jsx

--- src/pages/orderCheckout/paymentForm.jsx
+++ src/pages/orderCheckout/paymentForm.jsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
+import PropTypes from "prop-types";
 
-export default function PaymentForm() {
+export default function PaymentForm(props) {
 	return (
 		<React.Fragment>
 			<Typography variant="h6" gutterBottom>
@@ -18,6 +19,13 @@ export default function PaymentForm() {
 						fullWidth
 						autoComplete="cc-name"
 						variant="standard"
+						value={props.nameOnCard}
+						onChange={(event)=>{
+							const input = event.target.value;
+							if(input.length<25){
+								props.setNameOnCard(event.target.value);
+							}
+						}}
 					/>
 				</Grid>
 				<Grid item xs={12} md={6}>
@@ -28,6 +36,13 @@ export default function PaymentForm() {
 						fullWidth
 						autoComplete="cc-number"
 						variant="standard"
+						value={props.card}
+						onChange={(event)=>{
+							const input = event.target.value;
+							if(input.length<25) {
+								props.setCard(event.target.value);
+							}
+						}}
 					/>
 				</Grid>
 				<Grid item xs={12} md={6}>
@@ -38,6 +53,13 @@ export default function PaymentForm() {
 						fullWidth
 						autoComplete="cc-exp"
 						variant="standard"
+						value={props.expireDate}
+						onChange={(event)=>{
+							const input = event.target.value;
+							if(input.length<10){
+								props.setExpireDate(event.target.value);
+							}
+						}}
 					/>
 				</Grid>
 				<Grid item xs={12} md={6}>
@@ -49,9 +71,28 @@ export default function PaymentForm() {
 						fullWidth
 						autoComplete="cc-csc"
 						variant="standard"
+						value={props.cvv}
+						onChange={(event)=>{
+							const input = event.target.value;
+							if(input.length<6){
+								props.setCvv(input);
+								console.log(event.target.value);
+							}
+						}}
 					/>
 				</Grid>
 			</Grid>
 		</React.Fragment>
 	);
 }
+
+PaymentForm.propTypes = {
+	nameOnCard: PropTypes.string.isRequired,
+	setNameOnCard: PropTypes.func.isRequired,
+	card: PropTypes.string.isRequired,
+	setCard: PropTypes.func.isRequired,
+	expireDate: PropTypes.string.isRequired,
+	setExpireDate: PropTypes.func.isRequired,
+	cvv: PropTypes.string.isRequired,
+	setCvv: PropTypes.func.isRequired
+};
