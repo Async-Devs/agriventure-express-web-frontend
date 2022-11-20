046b4e925e46be06b62e47c1e0308ebf02fa2169
Achira Dias <achira828@gmail.com>
2022-11-21 00:28:12 +0530

    Delete App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Application Home Layout", () => {
-	render(<App />);
-	const linkElement = screen.getByTestId("App");
-	expect(linkElement).toBeInTheDocument();
-});
